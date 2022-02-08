77eb6ce70c0da1279b894d4baa0a240dad7d4243
Gilberto Junior <gagjunior@hotmail.com>
2022-02-08 00:15:33 -0300

    Aula 76 - Personalizando o botão e o padding

--- lib/main.dart
+++ lib/main.dart
@@ -38,19 +38,30 @@ class HomePage extends StatelessWidget {
             style: TextStyle(
                 fontSize: 30, color: Colors.white, fontWeight: FontWeight.w700),
           ),
-          const Text(
-            '0',
-            style: TextStyle(
-              fontSize: 100,
-              color: Colors.white,
-            ),
+          const Padding(
+              padding: EdgeInsets.all(40),
+              child: Text(
+                '0',
+                style: TextStyle(
+                  fontSize: 100,
+                  color: Colors.white,
+                ),
+              ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               TextButton(
                 onPressed: decrement,
-                child: Text(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: const Size(100, 100),
+                  primary: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                ),
+                child: const Text(
                   'Saiu',
                   style: TextStyle(
                     color: Colors.black,
@@ -58,9 +69,18 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
               ),
+              const SizedBox(width: 32,),
               TextButton(
                 onPressed: increment,
-                child: Text(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: const Size(100, 100),
+                  primary: Colors.black,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24)
+                  ),
+                ),
+                child: const Text(
                   'Entrou',
                   style: TextStyle(
                     color: Colors.black,
